65fd73c70e903d18c52900b138b0d2be482e5168
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-15 20:11:51 -0300

    Revert "secao-5 [Projeto Board tarefas]: Cadastrar tarefas, Cadastrando no banco de dados, Listando itens"
    This reverts commit 88bdd340c4f8f11a39efacbad9f2c04a1983d36f.

--- tarefas/src/pages/dashboard/index.tsx
+++ tarefas/src/pages/dashboard/index.tsx
@@ -5,83 +5,8 @@ import Head from 'next/head';
 import { getSession } from 'next-auth/react';
 import { Textarea } from '@/components/textarea';
 import { FaShare, FaTrash } from 'react-icons/fa';
-import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
-import { addDoc, collection, onSnapshot, orderBy, query, where } from 'firebase/firestore';
-import { db } from '@/services/firebaseConnection';
 
-interface HomeProps {
-    user: {
-        email: string
-    }
-}
-
-interface TasksProps {
-    id: string;
-    created: Date;
-    public: boolean;
-    tarefa: string;
-    user: string;
-}
-
-export default function Dashboard({ user }: HomeProps) {
-    const [input, setInput] = useState('');
-    const [publicTask, setPublicTask] = useState(false);
-    const [tasks, setTasks] = useState<TasksProps[]>([]);
-
-    useEffect(() => {
-        async function loadTarefas() {
-            const tarefasRef = collection(db, "tarefas");
-            const q = query(
-                tarefasRef,
-                orderBy("created", "desc"),
-                where("user", "==", user?.email)
-            )
-
-            onSnapshot(q, (snapshot) => {
-                let lista = [] as TasksProps[];
-
-                snapshot.forEach(doc => {
-                    lista.push({
-                        id: doc.id,
-                        tarefa: doc.data().tarefa,
-                        created: doc.data().created,
-                        user: doc.data().user,
-                        public: doc.data().public,
-                    })
-                })
-
-                setTasks(lista);
-            })
-        }
-
-        loadTarefas();
-    }, [user?.email])
-
-    function handleChangePublic(event: ChangeEvent<HTMLInputElement>) {
-        console.log(event.target.checked);
-        setPublicTask(event.target.checked)
-    }
-
-    async function handleRegisterTask(event: FormEvent) {
-        event.preventDefault();
-
-        if (input === '') return;
-
-        try {
-            await addDoc(collection(db, "tarefas"), {
-                tarefa: input,
-                created: new Date(),
-                user: user?.email,
-                public: publicTask
-            });
-
-            setInput('');
-            setPublicTask(false);
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
+export default function Dashboard() {
     return (
         <div className={styles.container}>
             <Head>
@@ -93,21 +18,10 @@ export default function Dashboard({ user }: HomeProps) {
                     <div className={styles.contentForm}>
                         <h1 className={styles.title}>Qual sua tarefa ?</h1>
 
-                        <form onSubmit={handleRegisterTask}>
-                            <Textarea
-                                placeholder='Digite qual sua tarefa ?'
-                                value={input}
-                                onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
-                                    setInput(event.target.value)
-                                }
-                            />
+                        <form>
+                            <Textarea placeholder='Digite qual sua tarefa ?' />
                             <div className={styles.checkboxArea}>
-                                <input
-                                    type="checkbox"
-                                    className={styles.checkbox}
-                                    checked={publicTask}
-                                    onChange={handleChangePublic}
-                                />
+                                <input type="checkbox" className={styles.checkbox} />
                                 <label>Deixar sua tarefa pública</label>
                             </div>
                             <button className={styles.button} type='submit'>
@@ -120,27 +34,23 @@ export default function Dashboard({ user }: HomeProps) {
                 <section className={styles.taskContainer}>
                     <h1>Minhas Tarefas</h1>
 
-                    {tasks.map((task) => (
-                        <article key={task.id} className={styles.task}>
-                            {task.public && (
-                                <div className={styles.tagContainer}>
-                                    <label className={styles.tag}>PUBLICO</label>
-                                    <button className={styles.shareButton}>
-                                        <FaShare
-                                            size={22}
-                                            color='#3183ff'
-                                        />
-                                    </button>
-                                </div>
-                            )}
-                            <div className={styles.taskContent}>
-                                <p>{task.tarefa}</p>
-                                <button className={styles.trashButton} type='submit'>
-                                    <FaTrash size={22} color='#ea3140' />
-                                </button>
-                            </div>
-                        </article>
-                    ))}
+                    <article className={styles.task}>
+                        <div className={styles.tagContainer}>
+                            <label className={styles.tag}>PUBLICO</label>
+                            <button className={styles.shareButton}>
+                                <FaShare
+                                    size={22}
+                                    color='#3183ff'
+                                />
+                            </button>
+                        </div>
+                        <div className={styles.taskContent}>
+                            <p>Minha primeira tarefa de exemplo!</p>
+                            <button className={styles.trashButton} type='submit'>
+                                <FaTrash size={22} color='#ea3140'/>
+                            </button>
+                        </div>
+                    </article>
                 </section>
             </main>
         </div>
@@ -160,10 +70,6 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     }
 
     return {
-        props: {
-            user: {
-                email: session?.user?.email
-            }
-        },
+        props: {},
     }
 }
