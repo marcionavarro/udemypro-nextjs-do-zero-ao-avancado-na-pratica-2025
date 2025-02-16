8b2edafe00b73e77c6dd0d49b5204272ffdb60e6
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-15 21:13:43 -0300

    secao-5 [Projeto Board tarefas]: Passando as secrets Firebase para env

--- tarefas/src/services/firebaseConnection.ts
+++ tarefas/src/services/firebaseConnection.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDcJ8bA4oyr6QSdMI3_2FxjOnfKxKBLzZY",
-  authDomain: "nextjstarefas.firebaseapp.com",
-  projectId: "nextjstarefas",
-  storageBucket: "nextjstarefas.firebasestorage.app",
-  messagingSenderId: "1045539010799",
-  appId: "1:1045539010799:web:3d6f9eedae33bf0eaa431a"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
