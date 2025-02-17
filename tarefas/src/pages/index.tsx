dbaa35144bdda1d2458980fa85a33a5bfb879069
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-17 14:38:55 -0300

    secao-5 [Projeto Board tarefas]: Criando páginas estáticas, Revendo projeto

--- tarefas/src/pages/index.tsx
+++ tarefas/src/pages/index.tsx
@@ -2,8 +2,16 @@ import Head from "next/head";
 import styles from "@/styles/home.module.css";
 import Image from "next/image";
 import heroImg from "./../../public/assets/hero.png";
+import { GetStaticProps } from "next";
+import { collection, getDoc, getDocs } from "firebase/firestore";
+import { db } from "@/services/firebaseConnection";
 
-export default function Home() {
+interface HomeProps {
+  posts: number;
+  comments: number;
+}
+
+export default function Home({posts, comments}: HomeProps) {
   return (
     <>
       <Head>
@@ -25,13 +33,29 @@ export default function Home() {
 
         <div className={styles.infoContent}>
           <section className={styles.box}>
-            <span>+12 posts</span>
+            <span>+{posts} posts</span>
           </section>
           <section className={styles.box}>
-            <span>+90 comentários</span>
+            <span>+{comments} comentários</span>
           </section>
         </div>
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const postRef = collection(db, "tarefas");
+  const commentRef = collection(db, "comments");
+
+  const postSnapShot = await getDocs(postRef);
+  const commentSnapsShot = await getDocs(commentRef);
+
+  return {
+    props: {
+      posts: postSnapShot.size || 0,
+      comments: commentSnapsShot.size || 0
+    },
+    revalidate: 60 // seria revalidada a cada 60seg
+  }
 }
