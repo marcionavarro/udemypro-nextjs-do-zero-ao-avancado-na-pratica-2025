a01b5b65a6065198969e43708b519fc607d88731
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-17 18:31:36 -0300

    secao-6 [Evoluindo com diretório app]: Criando novos projetos, Usando app router

---
+++ meuprojeto/src/app/contatos/page.tsx
@@ -0,0 +1,8 @@
+export default function Contatos() {
+    return (
+      <main>
+        <h1>Página Contatos</h1>
+        <span>(xx) 98888-7777</span>
+      </main>
+    );
+  }
---
+++ meuprojeto/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {  
+  return (
+    <main>
+      <h1>Página Home</h1>
+      <span>Seja bem vindo a página home</span>
+    </main>
+  );
+}
