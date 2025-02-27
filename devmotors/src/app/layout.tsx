e4056fa1de32108db5125bc2bec549d7c2167ae8
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-27 14:47:50 -0300

    secao-8 [Criando sistema completo + CMS]: Criando projeto

---
+++ devmotors/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.scss";
+import { Header } from "@/componentes/header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DevMotors - Sua oficina especializada",
+  description: "Oficina de carros em Uberlândia",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
