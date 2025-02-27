517f31e90d1046e9d9d441da67d412cc93cfccff
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-27 17:41:39 -0300

    secao-8 [Criando sistema completo + CMS]: Estilizando submenu, Configurando CMS administrativo, Revisando CMS

--- devmotors/src/componentes/home/submenu/index.tsx
+++ devmotors/src/componentes/home/submenu/index.tsx
@@ -1,16 +1,41 @@
+'use client'
+
 import Link from 'next/link'
 import styles from './styles.module.scss'
 import { X, Menu } from 'lucide-react'
+import { useEffect, useState } from 'react'
 
 export function Submenu() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    const hanleResize = () => {
+      if (window.innerWidth > 768) {
+        setIsOpen(false)
+      }
+    }
+
+    window.addEventListener("resize", hanleResize)
+    return window.removeEventListener("resize", hanleResize)
+  }, [])
+
+  function toggleMenu() {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <section className={styles.submenu}>
-      <div className={styles.submenuIcon}>
+      <div className={styles.submenuIcon} onClick={toggleMenu}>
         <Menu size={34} color='#121212' />
         Menu
       </div>
 
-      <ul className={styles.ul}>
+      <ul className={`${styles.ul} ${isOpen ? styles.open : ""}`}>
+        {isOpen && (
+          <button onClick={toggleMenu} className={styles.closeButton}>
+            <X size={54} color='#121212' />
+          </button>
+        )}
         <li>
           <Link href="/post/pagina-1">
             Pagina 1
