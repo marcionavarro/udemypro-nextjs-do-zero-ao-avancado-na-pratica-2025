67de08bebbd2ce21bd5f0cdf1bf5f3e5679bf53a
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-27 15:57:41 -0300

    secao-8 [Criando sistema completo + CMS]: Criando header, Esilizando header, Criando submenu de páginas

--- devmotors/src/app/page.tsx
+++ devmotors/src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Submenu } from "@/componentes/home/submenu";
+
 export default function Home() {
   return (
     <main>
-      <h1>TESTE PAGINA</h1>
+      <Submenu />
     </main>
   );
 }
--- devmotors/src/componentes/header/index.tsx
+++ devmotors/src/componentes/header/index.tsx
@@ -1,9 +1,37 @@
-import styles from './styles.module.scss'
+'use client'
 
-export function Header () {
+import Link from 'next/link'
+import styles from './styles.module.scss'
+import { useEffect, useState } from 'react'
+
+export function Header() {
+  const [top, setTop] = useState(true)
+
+  const scrollHandler = () => {
+    window.scrollY > 10 ? setTop(false) : setTop(true)
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler)
+
+    return () => window.removeEventListener("scroll", scrollHandler)
+  }, [top])
+
   return (
-    <header>
-      <h1>Header</h1>
+    <header className={`${styles.header} ${!top ? styles.fixed : styles.background}`}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.contentLogo}>
+            <Link href='/' title="Logo images">Dev Motors</Link>
+          </div>
+
+          <nav className={styles.nav}>
+            <Link href='/'>Home</Link>
+            <Link href='/#servicos'>Serviços</Link>
+            <Link href='/#contatos'>Contatos</Link>
+          </nav>
+        </div>
+      </div>
     </header>
   )
 }
