c8130363ecaf9776c91cfe3e116244f68a3b792c
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-03-02 18:36:58 -0300

    secao-9 [Projeto Dev Controle sistema completo]: Criando a pagina home, Conhecendo next auth, Configurando autenticação de usuários, Finalizando configuração auth

--- devcontrole/src/app/page.tsx
+++ devcontrole/src/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import heroImg from '@/assets/hero.svg'
 
 export default function Home() {
   return (
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Teste</h1>
-      </main>
+    <main className="flex flex-col justify-center items-center min-h-[calc(100vh-80px)]">
+      <h2 className="font-medium text-2xl mb-2">Gerencie sua empresa</h2>
+      <h1 className="font-bold text-3xl mb:text-4xl mb-8 text-blue-600">Atendimentos, clientes</h1>
+      <Image
+        src={heroImg}
+        alt="Imagem hero do dev controle"
+        className="max-w-sm md:max-w-xl"
+      />
+    </main>
   );
 }
